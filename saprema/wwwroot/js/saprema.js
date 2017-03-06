039c28391d880b3585fd7c97e7793644fb9b8741
Seamus O'Higgins <s.o.higgins1986@gmail.com>
2017-03-06 00:24:41 +0000

    More front-end navigation completed, work started on breath timer.

--- saprema/wwwroot/js/saprema.js
+++ saprema/wwwroot/js/saprema.js
@@ -1,4 +1,7 @@
-﻿// Delegates for login/sign-up
+﻿/* Delegates for right navigation options */
+window.onload = loadHtml("/saprema-views/nav-login-signup.html", "nav-right", "GET", "text/html");
+
+/* Delegates for login/sign-up */
 delegate(document, "click", "#user-register-submit", function (event) {
     event.preventDefault();
     //var form = document.getElementById("register-form");
@@ -13,6 +16,8 @@ delegate(document, "click", "#user-login-submit", function (event) {
     //var params = serialize(form);
     //loadHtml("/modal-login", "container", "POST", "application/json", params);
     loadHtml("/saprema-views/main-menu.html", "main-content", "GET", "text/html");
+    //Set following code up to exicute on login success
+    loadHtml("/saprema-views/nav-logged-in.html", "nav-right", "GET", "text/html");
 });
 
 delegate(document, "click", "#user-login-btn", function (event) {
@@ -31,6 +36,40 @@ delegate(document, "click", "#student-radio", function (event) {
 
 delegate(document, "click", "#teacher-radio", function (event) {
     loadHtml("/saprema-views/register-teacher.html", "register-level", "GET", "text/html");
+});
+
+/* Delegates for user drop down */
+//Logout functions go here
+delegate(document, "click", "#logout-btn", function (event) {
+    loadHtml("/saprema-views/nav-login-signup.html", "nav-right", "GET", "text/html");
+});
+
+delegate(document, "click", "#profile-btn", function (event) {
+    loadHtml("/saprema-views/user-profile.html", "main-content", "GET", "text/html");
+});
+
+delegate(document, "click", "#statistics-btn", function (event) {
+    loadHtml("/saprema-views/user-stats.html", "main-content", "GET", "text/html");
+});
+
+delegate(document, "click", "#poses-btn", function (event) {
+    loadHtml("/saprema-views/user-poses.html", "main-content", "GET", "text/html");
+});
+
+/* Delegates for meditation menu */
+delegate(document, "click", "#meditation-menu-breath", function (event) {
+    loadHtml("/saprema-views/meditation-breath.html", "main-content", "GET", "text/html");
+});
+
+delegate(document, "click", "#breath-play-btn", function (event) {
+    var breath_time = [];
+    breath_time[0] = parseInt(document.getElementById("inhale").value);
+    breath_time[1] = parseInt(document.getElementById("inHold").value);
+    breath_time[2] = parseInt(document.getElementById("exhale").value);
+    breath_time[3] = parseInt(document.getElementById("outHold").value);
+    //var params = serialize(breath_time);
+    loadHtml("/saprema-views/meditation-breath-play.html", "main-content", "POST", "application/json", breath_time);
+    //loadHtml("/saprema-views/meditation-breath-play.html", "main-content", "GET", "text/html");
 });
 
 function loadHtml(route, replace, type, reqHeader, params) {
@@ -61,7 +100,7 @@ function loadHtml(route, replace, type, reqHeader, params) {
     }
 }
 
-// Function code taken from Stackoverflow https://stackoverflow.com/questions/30880757/javascript-equivalent-to-on
+//Function code taken from Stackoverflow https://stackoverflow.com/questions/30880757/javascript-equivalent-to-on
 function delegate(el, evt, sel, handler) {
     el.addEventListener(evt, function (event) {
         var t = event.target;
